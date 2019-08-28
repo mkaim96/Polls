b3ab58d27cf51d46392384030a68e0d0c210de82
Marcin <marcinkaim96@gmail.com>
2019-08-28 20:36:07 +0100

    Add functionality to copy link to poll

---
+++ Polls.Mvc/wwwroot/js/CopyLinkToForm.js
@@ -0,0 +1,18 @@
+﻿let copyBtn = document.getElementById('copy');
+
+let url = window.location.href;
+
+let urlArr = url.split("/");
+let id = urlArr[urlArr.length - 1];
+
+
+let urlToCopy = `https://localhost:44392/polls/form/${id}`;
+
+let input = document.getElementById('link');
+
+input.value = urlToCopy;
+
+copyBtn.addEventListener('click', function() {
+    input.select();
+    document.execCommand('copy');   
+})
