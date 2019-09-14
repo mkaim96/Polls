2ff343253ba80265700ce60fcf9ce98974478a61
Marcin <marcinkaim96@gmail.com>
2019-09-14 19:03:30 +0100

    Revert "Delete unused service"
    This reverts commit 83822087760d8f8cbe94e10a4709509ce38dc288.

---
+++ Polls.Infrastructure/Services/Interfaces/IPollsService.cs
@@ -0,0 +1,15 @@
+﻿using Polls.Infrastructure.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polls.Infrastructure.Services.Interfaces
+{
+    public interface IPollsService
+    {
+        Task<IEnumerable<PollDto>> GetAll(string userId);
+        Task<PollDto> Get(int id);
+        Task Delete(int id);
+    }
+}
