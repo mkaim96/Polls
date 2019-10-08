b7005e1977335fea5cb45235e15cecadb131a578
Marcin <marcinkaim96@gmail.com>
2019-10-08 16:47:31 +0100

    Use automapperc and pass domain objects to repositories insted of Dto's

--- Polls.Infrastructure/Handlers/Commands/Polls/EditPollHandler.cs
+++ Polls.Infrastructure/Handlers/Commands/Polls/EditPollHandler.cs
@@ -12,51 +12,62 @@ using System.Data;
 using Polls.Infrastructure.Repositories;
 using Polls.Core.Domain;
 using Polls.Infrastructure.UnitOfWork;
+using AutoMapper;
 
 namespace Polls.Infrastructure.Handlers.Commands.Polls
 {
     public class EditPollHandler : AsyncRequestHandler<EditPoll>
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-        public EditPollHandler(IUnitOfWork uow)
+        public EditPollHandler(IUnitOfWork uow, IMapper mapper)
         {
             _unitOfWork = uow;
+            _mapper = mapper;
         }
         protected override async Task Handle(EditPoll request, CancellationToken cancellationToken)
         {
 
             var tasks = new List<Task>();
+
+            #region Deleting questions
             // Delete single choice questions.
             if (request.ScQuestionsToDelete.Count > 0)
             {
-                var t1 = _unitOfWork.Questions.DeleteMany(request.ScQuestionsToDelete);
+                var questions = _mapper.Map<IEnumerable<SingleChoiceQuestion>>(request.ScQuestionsToDelete);
+
+                var t1 = _unitOfWork.Questions.DeleteMany(questions);
                 tasks.Add(t1);
             }
 
             // Delete text asnwer questions.
             if (request.TaQuestionsToDelete.Count > 0)
             {
-                var t2 = _unitOfWork.Questions.DeleteMany(request.TaQuestionsToDelete);
+                var questions = _mapper.Map<IEnumerable<TextAnswerQuestion>>(request.TaQuestionsToDelete);
+
+                var t2 = _unitOfWork.Questions.DeleteMany(questions);
                 tasks.Add(t2);
             }
 
             // Delete multiple choice questions.
             if(request.McQuestionsToDelete.Count > 0)
             {
-                var t3 = _unitOfWork.Questions.DeleteMany(request.McQuestionsToDelete);
-            }
+                var questions = _mapper.Map<IEnumerable<MultipleChoiceQuestion>>(request.McQuestionsToDelete);
 
+                var t3 = _unitOfWork.Questions.DeleteMany(questions);
+            }
+            #endregion
+
+            #region Inserting new questions
             // Insert single choice questions.
             if (request.NewScQuestions.Count > 0)
             {
                 // Prepare questions.
-                var questions = request.NewScQuestions.Select(x =>
-                {
-                    return new SingleChoiceQuestion(
-                        request.PollId, x.QuestionType, x.QuestionText, x.Number, x.Choices
-                        );
-                });
+                var questions = request.NewScQuestions.Select(x => new SingleChoiceQuestion(
+                    request.PollId, x.QuestionType, x.QuestionText, x.Number, x.Choices
+                    )
+                );
 
                 var t4 = _unitOfWork.Questions.InsertMany(questions);
                 tasks.Add(t4);
@@ -66,12 +77,10 @@ namespace Polls.Infrastructure.Handlers.Commands.Polls
             if (request.NewTaQuestions.Count > 0)
             {
                 // Prepare questions.
-                var questions = request.NewTaQuestions.Select(x =>
-                {
-                    return new TextAnswerQuestion(
+                var questions = request.NewTaQuestions.Select(x => new TextAnswerQuestion(
                         request.PollId, x.QuestionType, x.QuestionText, x.Number
-                        );
-                });
+                        )
+                );
 
                 var t5 = _unitOfWork.Questions.InsertMany(questions);
                 tasks.Add(t5);
@@ -81,26 +90,32 @@ namespace Polls.Infrastructure.Handlers.Commands.Polls
             if(request.NewMcQuestions.Count > 0)
             {
                 // Prepare questions.
-                var questions = request.NewMcQuestions.Select(x =>
-                {
-                    return new MultipleChoiceQuestion(request.PollId, x.QuestionType, x.QuestionText, x.Number, x.Choices);
-                });
+                var questions = request.NewMcQuestions.Select(x => new MultipleChoiceQuestion(
+                        request.PollId, x.QuestionType, x.QuestionText, x.Number, x.Choices
+                        )
+                );
 
                 var t6 = _unitOfWork.Questions.InsertMany(questions);
                 tasks.Add(t6);
             }
+            #endregion
 
+            #region Updating questions
             // Update Single Choice Questions.
             if(request.ScQuestionsToUpdate.Count > 0)
             {
-                var t7 = _unitOfWork.Questions.UpdateMany(request.ScQuestionsToUpdate);
+                var questions = _mapper.Map<IEnumerable<SingleChoiceQuestion>>(request.ScQuestionsToUpdate);
+
+                var t7 = _unitOfWork.Questions.UpdateMany(questions);
                 tasks.Add(t7);
             }
 
             // Update Text Answer Questions.
             if(request.TaQuestionsToUpdate.Count > 0)
             {
-                var t8 = _unitOfWork.Questions.UpdateMany(request.TaQuestionsToUpdate);
+                var questions = _mapper.Map<IEnumerable<TextAnswerQuestion>>(request.TaQuestionsToUpdate);
+
+                var t8 = _unitOfWork.Questions.UpdateMany(questions);
                 tasks.Add(t8);
 
             }
@@ -108,9 +123,12 @@ namespace Polls.Infrastructure.Handlers.Commands.Polls
             // Update multiple choice questions.
             if(request.McQuestionsToUpdate.Count > 0)
             {
-                var t9 = _unitOfWork.Questions.UpdateMany(request.McQuestionsToUpdate);
+                var questions = _mapper.Map<IEnumerable<MultipleChoiceQuestion>>(request.McQuestionsToUpdate);
+
+                var t9 = _unitOfWork.Questions.UpdateMany(questions);
                 tasks.Add(t9);
             }
+            #endregion
 
             // Update poll.
             var t10 = _unitOfWork.Polls.Update(request.PollId, request.NewTitle, request.NewDescription);
