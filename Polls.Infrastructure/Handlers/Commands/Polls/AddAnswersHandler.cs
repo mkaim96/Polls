f5c9e31f403c0b88bd05985ba26dcaaeb2be61dd
Marcin <marcinkaim96@gmail.com>
2019-10-23 22:54:20 +0100

    use pattern matching when looping through questions

--- Polls.Infrastructure/Handlers/Commands/Polls/AddAnswersHandler.cs
+++ Polls.Infrastructure/Handlers/Commands/Polls/AddAnswersHandler.cs
@@ -28,59 +28,56 @@ namespace Polls.Infrastructure.Handlers.Commands.Polls
 
             var poll = await _unitOfWork.Polls.Get(pollId);
 
-            var scQuestions = poll.GetConcreteQuestions<SingleChoiceQuestion>();
-            var taQuestions = poll.GetConcreteQuestions<TextAnswerQuestion>();
-            var mcQuestions = poll.GetConcreteQuestions<MultipleChoiceQuestion>();
-
-
             // Initialize lists of answers.
             var scQuestionAnswers = new List<SingleChoiceAnswer>();
             var taQuestionAnswers = new List<TextAnswer>();
             var mcQuestionAnswers = new List<MultipleChoiceAnswer>();
 
-            // Loop through every single choice question and and check if form contains answer for it
-            // if yes, create answer and add to list.
-            foreach (var question in scQuestions)
+            // Loop through questions and depending on the type create answer for it 
+            foreach (var question in poll.Questions)
             {
-                if (request.Form.ContainsKey(question.Id.ToString()))
+                switch (question)
                 {
-                    scQuestionAnswers.Add(new SingleChoiceAnswer
-                    {
-                        QuestionId = question.Id,
-                        Choice = request.Form[question.Id.ToString()],
-                    });
-                }
-            }
-            
-            foreach (var question in mcQuestions)
-            {
-                if(request.Form.ContainsKey(question.Id.ToString()))
-                {
-                    mcQuestionAnswers.Add(new MultipleChoiceAnswer
-                    {
-                        QuestionId = question.Id,
-                        Choices = request.Form[question.Id.ToString()]
-                    });
-                }
-            }
+                    case SingleChoiceQuestion q:
 
-            // Loop through every text answer question and and check if form contains answer for it
-            // if yes, create answer and add to list.
-            foreach (var question in taQuestions)
-            {
-                if (request.Form.ContainsKey(question.Id.ToString()))
-                {
-                    // Check if answer isnt empty
-                    if (String.IsNullOrEmpty(request.Form[question.Id.ToString()]))
-                    {
-                        continue;
-                    }
+                        // Check if passed answers contains answer for this question
+                        if (request.Form.ContainsKey(q.Id.ToString()))
+                        {
+                            scQuestionAnswers.Add(new SingleChoiceAnswer
+                            {
+                                QuestionId = q.Id,
+                                Choice = request.Form[question.Id.ToString()],
+                            });
+                        }
+                        break;
 
-                    taQuestionAnswers.Add(new TextAnswer
-                    {
-                        QuestionId = question.Id,
-                        Answer = request.Form[question.Id.ToString()]
-                    });
+                    case MultipleChoiceQuestion q:
+                        if (request.Form.ContainsKey(q.Id.ToString()))
+                        {
+                            mcQuestionAnswers.Add(new MultipleChoiceAnswer
+                            {
+                                QuestionId = q.Id,
+                                Choices = request.Form[q.Id.ToString()]
+                            });
+                        }
+                        break;
+
+                    case TextAnswerQuestion q:
+                        if (request.Form.ContainsKey(q.Id.ToString()))
+                        {
+                            // Check if answer isnt empty
+                            if (String.IsNullOrEmpty(request.Form[q.Id.ToString()]))
+                            {
+                                continue;
+                            }
+
+                            taQuestionAnswers.Add(new TextAnswer
+                            {
+                                QuestionId = q.Id,
+                                Answer = request.Form[q.Id.ToString()]
+                            });
+                        }
+                        break;
                 }
             }
 
