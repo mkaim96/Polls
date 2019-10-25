14ccc3dc874a29c21b287c5b2bdc59ca743d8381
Marcin <marcinkaim96@gmail.com>
2019-10-25 12:39:25 +0100

    Add tests

--- Polls.Tests/SingleChoiceQuestionStatsGenerator.cs
+++ Polls.Tests/SingleChoiceQuestionStatsGenerator.cs
@@ -10,16 +10,16 @@ namespace Polls.Tests
 {
     public class SingleChoiceQuestionStatsGenerator
     {
-        [Theory]
-        [InlineData("opcja 1", 2)]
-        [InlineData("opcja 2", 1)]
-        [InlineData("opcja 3", 1)]
-        public void should_count_votes(string choice, int votes)
+        SingleChoiceQuestionStatisticsGenerator statsGenerator;
+        SingleChoiceQuestion question;
+        List<Answer> answers;
+
+        public SingleChoiceQuestionStatsGenerator()
         {
-            // Arange.
-            var statGenerator = new SingleChoiceQuestionStatisticsGenerator();
-            var question = new SingleChoiceQuestion(1, "test question", 1, new string[] { "opcja 1", "opcja 2", "opcja 3" });
-            var answers = new List<Answer>
+            // Arrange.
+            statsGenerator = new SingleChoiceQuestionStatisticsGenerator();
+            question = new SingleChoiceQuestion(1, "test question", 1, new string[] { "opcja 1", "opcja 2", "opcja 3" });
+            answers = new List<Answer>
             {
                 new SingleChoiceAnswer {Id = 1, Choice = "opcja 1", QuestionId = question.Id },
                 new SingleChoiceAnswer {Id = 2, Choice = "opcja 2", QuestionId = question.Id },
@@ -27,9 +27,41 @@ namespace Polls.Tests
                 new SingleChoiceAnswer {Id = 4, Choice = "opcja 1", QuestionId = question.Id },
 
             };
+        }
+
+        [Fact]
+        public void wrong_quetion_type_should_throw_exception()
+        {
+            // Arrange.
+            var wrongQuestion = new TextAnswerQuestion(1, "test question", 1);
+
+            // Act/Assert.
+            Assert.Throws<ArgumentException>(() => statsGenerator.Generate(wrongQuestion, answers));
+        }
+
+        [Fact]
+        public void should_count_answers()
+        {
+            // Arange.
+            var expected = answers.Count;
 
             // Act.
-            var stats = (SinlgeChoiceQuestionStatistics)statGenerator.Generate(question, answers);
+            var stats = (SinlgeChoiceQuestionStatistics)statsGenerator.Generate(question, answers);
+
+            // Assert.
+            var actual = stats.VotesCount;
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Theory]
+        [InlineData("opcja 1", 2)]
+        [InlineData("opcja 2", 1)]
+        [InlineData("opcja 3", 1)]
+        public void should_count_votes(string choice, int votes)
+        {
+            // Act.
+            var stats = (SinlgeChoiceQuestionStatistics)statsGenerator.Generate(question, answers);
 
             // Assert.
             var actual = stats.ChoicesCount[choice];
